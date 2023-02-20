c279719b5e880ae8dc278220157e52a560b7743f
Taileduser <aalex.03@mail.ru>
2023-02-20 23:47:15 +0300

    update

--- app/src/main/java/com/example/tourismapp/MainActivity.java
+++ app/src/main/java/com/example/tourismapp/MainActivity.java
@@ -1,79 +1,16 @@
 package com.example.tourismapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.util.Log;
-import android.widget.Toast;
+import android.view.Window;
 
 import android.os.Bundle;
 
-
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "TourismApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
-        Context context = getApplicationContext();
-        CharSequence lifecycle = "onCreate";
-        Toast.makeText(context, lifecycle, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, lifecycle.toString());
-        Log.e(TAG, lifecycle.toString() + "Err");
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Context context = getApplicationContext();
-        CharSequence lifecycle = "onStart";
-        Toast.makeText(context, lifecycle, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, lifecycle.toString());
-        Log.e(TAG, lifecycle.toString() + "Err");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Context context = getApplicationContext();
-        CharSequence lifecycle = "onStop";
-        Toast.makeText(context, lifecycle, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, lifecycle.toString());
-        Log.e(TAG, lifecycle.toString() + "Err");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Context context = getApplicationContext();
-        CharSequence lifecycle = "onDestroy";
-        Toast.makeText(context, lifecycle, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, lifecycle.toString());
-        Log.e(TAG, lifecycle.toString() + "Err");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Context context = getApplicationContext();
-        CharSequence lifecycle = "onPause";
-        Toast.makeText(context, lifecycle, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, lifecycle.toString());
-        Log.e(TAG, lifecycle.toString() + "Err");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Context context = getApplicationContext();
-        CharSequence lifecycle = "onResume";
-        Toast.makeText(context, lifecycle, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, lifecycle.toString());
-        Log.e(TAG, lifecycle.toString() + "Err");
-    }
-
-
 }
