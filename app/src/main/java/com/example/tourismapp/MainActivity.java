e5bc95a99367d9ff68a8d5f455c29aa94a04f9e3
Taileduser <aalex.03@mail.ru>
2023-02-22 15:12:55 +0300

    ActionListener added

--- app/src/main/java/com/example/tourismapp/MainActivity.java
+++ app/src/main/java/com/example/tourismapp/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.tourismapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "TourismApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        
+        TextView myTextView = findViewById(R.id.textView4);
+        ImageView myImageView = findViewById(R.id.imageView);
+
+        Button myButton = findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Tourism App", "Button clicked with programing method");
+            }
+        });
+    }
+
+    public void myButtonOnClickListener(View view) {
+        Log.d(TAG, "Button clicked with daclaration method");
     }
 }
